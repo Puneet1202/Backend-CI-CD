b5c78032e2b54c73279911862f0c00c0f1656ea4
Punit <punitkumar2121999@gmail.com>
2026-03-03 20:51:31 +0530

    Added Jest testing and fixed CI/CD paths

--- Backend/src/app.test.js
+++ Backend/src/app.test.js
@@ -1,3 +1,11 @@
-test('Basic test - sab sahi hai', () => {
-  expect(1 + 1).toBe(2);
+  const request = require('supertest');
+const app = require('./app.js'); // Aapki main express file (jahan app.get/post hain)
+
+describe('API Testing', () => {
+  test('Check All Posts API', async () => {
+    const res = await request(app).get('/showAll-post');
+    expect(res.statusCode).toBe(200); // Check karo status 200 hai ya nahi
+  });
 });
+ 
+
